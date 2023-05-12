5c83084739f9ccefd996a7e9d50905a5cf5a2534
Jerry Chen <jerry@apache.org>
2023-05-12 18:19:12 -0500

    Fix set-version.sh for BSD sed

--- Release/set-version.sh
+++ Release/set-version.sh
@@ -11,18 +11,18 @@ echo "current version: ${version}"
 echo "    new version: ${new_version}"
 
 # Update Info.plist
-sed -i'' -e 's;'${version}';'${new_version}';g' ../Sky/Info.plist
+sed -i '' -e 's;'${version}';'${new_version}';g' ../Sky/Info.plist
 
 if [ -z "$(echo ${new_version} | grep 'pre')" ]; then
     # production release things
 
     # markdown in sed? what a country!
     markdown='The current release is [**'${new_version}'**](https://github.com/jcsalterego/Sky.app/releases/latest).'
-    sed -i'' \
+    sed -i '' \
         -e 's^The current release is .*^'"${markdown}"'^g' \
         ../README.md
 
-    sed -i'' \
+    sed -i '' \
         -e 's^'${version}'^'${new_version}'^g' \
         ../README.md
 fi
