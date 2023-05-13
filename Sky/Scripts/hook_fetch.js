9b110a50bcd08bbb24a93002827842389047fffa
Jerry Chen <jerry@apache.org>
2023-05-13 08:55:35 -0500

    FIX: Wrap hookFetch in try/catch

--- Sky/Scripts/hook_fetch.js
+++ Sky/Scripts/hook_fetch.js
@@ -3,21 +3,23 @@
     window.fetch = async function(...args) {
         let url = args[0];
         const response = await originalFetch(...args);
-        let responseData = await response.clone().json();
-        window.webkit.messageHandlers.fetch.postMessage({
-            "url": url,
-            "response": responseData
-        });
-
-        if (url.indexOf('https://search.bsky.social/search/posts') === 0
-            && document.body.dataset.featureOrderPosts === 'yes'
-        ) {
-            responseData.sort((post2, post1) => post1.post.createdAt < post2.post.createdAt ? -1 : 1)
-            response.json = async function() {
-                return responseData;
+        try {
+            let responseData = await response.clone().json();
+            window.webkit.messageHandlers.fetch.postMessage({
+                "url": url,
+                "response": responseData
+            });
+            if (url.indexOf('https://search.bsky.social/search/posts') === 0
+                && document.body.dataset.featureOrderPosts === 'yes'
+                ) {
+                responseData.sort((post2, post1) => post1.post.createdAt < post2.post.createdAt ? -1 : 1)
+                response.json = async function() {
+                    return responseData;
+                }
             }
+        } catch (error) {
+            console.warn("Failed to hook_fetch", error);
         }
-
         return response;
     }
 })();
