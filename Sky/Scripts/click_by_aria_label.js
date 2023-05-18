900404469ab0b44dfbedebc5ada262cea3e5c870
Jerry Chen <jerry@apache.org>
2023-05-18 09:50:59 -0500

    Prettier click_by_aria_label.js

--- Sky/Scripts/click_by_aria_label.js
+++ Sky/Scripts/click_by_aria_label.js
@@ -1,6 +1,8 @@
 $INCLUDE("_filter_visible.js");
 function clickByAriaLabel(ariaLabel) {
-    let elems = filterVisible(document.querySelectorAll(`[aria-label^="${ariaLabel}" i]`));
+    let elems = filterVisible(
+        document.querySelectorAll(`[aria-label^="${ariaLabel}" i]`)
+    );
     elems.forEach((elem) => elem.click());
 }
 clickByAriaLabel("$__ARIA_LABEL__");
