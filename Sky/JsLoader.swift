2dc5faa426447ffaaeb361fb14a8ce56c9c1f560
Jerry Chen <jerry@apache.org>
2023-05-18 09:45:17 -0500

    JsLoader supports $INCLUDE

--- Sky/JsLoader.swift
+++ Sky/JsLoader.swift
@@ -19,6 +19,21 @@ class JsLoader {
         )
     }
 
+    class func resolveIncludePath(_ includeFile: String, referencePath: String) -> String {
+        var resolvedPath = includeFile
+        if !includeFile.contains("/") {
+            let words = referencePath.split(separator: "/")
+            if words.count > 1 {
+                var slice = words[..<(words.count-1)].compactMap { substring in
+                    String(substring)
+                }
+                slice.append(includeFile)
+                resolvedPath = slice.joined(separator: "/")
+            }
+        }
+        return resolvedPath
+    }
+
     class func loadScriptContents(
         _ name: String,
         _ context: [String: String] = [:]
@@ -34,6 +49,34 @@ class JsLoader {
                 of: "$LOG",
                 with: "window.webkit.messageHandlers.consoleLog.postMessage"
             )
+        }
+        if myText.contains("$INCLUDE") {
+            var finalLines: [String] = []
+            let lines = myText.components(separatedBy: "\n")
+            for idx in lines.indices {
+                var found = false
+                let line = lines[idx]
+                if let regex = try? NSRegularExpression(pattern: "\\$INCLUDE\\(['\"](.+)['\"]\\)", options: []) {
+                    let nsRange = NSRange(myText.startIndex..., in: line)
+                    let matches = regex.matches(in: line, range: nsRange)
+                    for match in matches {
+                        let firstMatchRange = match.range(at: 1)
+                        let swiftRange = Range(firstMatchRange, in: myText)
+                        let includeFile = myText[swiftRange!]
+                        var includePath = resolveIncludePath(String(includeFile), referencePath: name)
+                        includePath = includePath.replacingOccurrences(of: ".js", with: "")
+                        let innerScript = loadScriptContents(includePath)
+                        for innerScriptLine in innerScript.components(separatedBy: "\n") {
+                            finalLines.append(innerScriptLine)
+                        }
+                        found = true
+                    }
+                }
+                if !found {
+                    finalLines.append(line)
+                }
+            }
+            myText = finalLines.joined(separator: "\n")
         }
         return myText
     }
