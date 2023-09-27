d58ebaa394430d15c1efbc48baa8cdb16026dd65
Jerry Chen <jerry@apache.org>
2023-09-27 10:08:59 -0500

    Extra filter for aria-label

--- Sky/Scripts/_find_all_by_aria_label.js
+++ Sky/Scripts/_find_all_by_aria_label.js
@@ -2,5 +2,9 @@ $INCLUDE("_filter_visible.js");
 function findAllByAriaLabel(ariaLabel) {
     return filterVisible(
         document.querySelectorAll(`[aria-label^="${ariaLabel}" i]`)
+    ).filter(
+        (elem) => {
+            return elem.getAttribute("aria-label").toLowerCase() === ariaLabel.toLowerCase();
+        }
     );
 }
