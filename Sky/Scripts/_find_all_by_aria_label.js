5f2e23e226ddaf442c0ac25bd7e1d680388ac76d
Jerry Chen <jerry@apache.org>
2024-03-17 07:11:59 -0500

    findAllByAriaLabel gets visibleOnly=false

--- Sky/Scripts/_find_all_by_aria_label.js
+++ Sky/Scripts/_find_all_by_aria_label.js
@@ -1,10 +1,16 @@
 $INCLUDE("_filter_visible.js");
-function findAllByAriaLabel(ariaLabel) {
-    return filterVisible(
-        document.querySelectorAll(`[aria-label^="${ariaLabel}" i]`)
-    ).filter(
+
+function findAllByAriaLabel(ariaLabel, visibleOnly = false) {
+    let elems = document.querySelectorAll(`[aria-label^="${ariaLabel}" i]`);
+    if (visibleOnly) {
+        elems = filterVisible(elems);
+    } else {
+        elems = Array.from(elems);
+    }
+    elems = elems.filter(
         (elem) => {
             return elem.getAttribute("aria-label").toLowerCase() === ariaLabel.toLowerCase();
         }
     );
+    return elems;
 }
