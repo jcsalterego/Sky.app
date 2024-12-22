ba86a9c862bea1eae40bf3fbefe4cffa41e2c1c9
Jerry Chen <jerry@apache.org>
2024-12-22 10:16:56 -0600

    withRetry gets flexible maxRetries

--- Sky/Scripts/_with_retry.js
+++ Sky/Scripts/_with_retry.js
@@ -1,13 +1,16 @@
-function withRetry(fn, retryMs = 200, count = 1) {
-    const MAX_RETRIES = 3;
-    if (count >= MAX_RETRIES) {
+function withRetry(fn, retryMs = 200, maxRetries = null, count = 1) {
+    const DEFAULT_MAX_RETRIES = 3;
+    if (!maxRetries) {
+        maxRetries = DEFAULT_MAX_RETRIES;
+    }
+    if (count >= maxRetries) {
         console.error(`Failed after ${count} tries. Aborting`);
     } else {
         let done = fn();
         const fnSig = fn.toString().split("\n")[0].substring(0, 50);
         if (!done) {
             $LOG(`failed "${fnSig}...". retrying in ${retryMs}`);
-            window.setTimeout(() => withRetry(fn, retryMs, count + 1), retryMs);
+            window.setTimeout(() => withRetry(fn, retryMs, maxRetries, count + 1), retryMs);
         } else if (count > 1) {
             $LOG(`succeeded at try ${count}: "${fnSig}..."`);
         }
