4555fe38b6cea396d920c4a48211f2a2aef5d69c
Jerry Chen <jerry@apache.org>
2024-12-28 10:03:51 -0600

    hook_window_color_scheme uses initSystemAppearance

--- Sky/Scripts/hook_window_color_scheme.js
+++ Sky/Scripts/hook_window_color_scheme.js
@@ -1,5 +1,77 @@
 $INCLUDE("_with_retry");
+
+function getBskyStorage() {
+    let bskyStorage = null;
+    try {
+        bskyStorage = JSON.parse(window.localStorage.BSKY_STORAGE);
+    } catch (e) {
+        console.error("Could not load BSKY_STORAGE", e);
+    }
+    return bskyStorage;
+}
+
 function updateColorScheme() {
+    let bskyStorage = getBskyStorage();
+    if (bskyStorage) {
+        updateColorSchemeWithBskyStorage(bskyStorage);
+    } else {
+        updateColorSchemeAuto();
+    }
+}
+
+function getInitSystemAppearance() {
+    return localStorage.initSystemAppearance;
+}
+
+function updateColorSchemeWithBskyStorage(bskyStorage) {
+    const colorMode = bskyStorage.colorMode;
+    const darkTheme = bskyStorage.darkTheme;
+    const classDefs = {
+        "theme--light": {
+            "name": "light",
+            "darkMode": false,
+            "backgroundColor": "rgb(255, 255, 255)",
+        },
+        "theme--dark": {
+            "name": "dark",
+            "darkMode": true,
+            "backgroundColor": "rgb(0, 0, 0)",
+        },
+        "theme--dim": {
+            "name": "dim",
+            "darkMode": true,
+            "backgroundColor": "rgb(24, 30, 38)",
+        }
+    };
+    let match = classDefs["theme--light"];
+    let initSystemAppearance = getInitSystemAppearance();
+    console.info("initSystemAppearance", initSystemAppearance);
+    if (colorMode === "system") {
+        if (initSystemAppearance === "dark" && darkTheme === "dark") {
+            match = classDefs["theme--dark"];
+        } else if (initSystemAppearance === "dark" && darkTheme === "dim") {
+            match = classDefs["theme--dim"];
+        } else {
+            match = classDefs["theme--light"];
+        }
+    } else {
+        let htmlTheme = getHtmlTheme();
+        for (let c of Object.keys(classDefs)) {
+            if (htmlTheme === c) {
+                match = classDefs[c];
+                break;
+            }
+        }
+    }
+    window.webkit.messageHandlers.windowColorSchemeChange.postMessage({
+        colorScheme: match.name,
+        darkMode: match.darkMode,
+        backgroundColor: match.backgroundColor,
+    });
+    return true;
+}
+
+function updateColorSchemeAuto() {
     const classDefs = {
         "theme--light": {
             "name": "light",
