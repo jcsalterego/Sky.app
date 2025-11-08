b0baad07908c567b9c8f8b06485ef9c83776b2ba
Jerry Chen <jerry@apache.org>
2025-11-08 07:22:07 -0600

    Add MainWindowController

---
+++ Sky/MainWindowController.swift
@@ -0,0 +1,14 @@
+//
+//  MainWindowController
+//  Sky
+//
+
+import AppKit
+
+class MainWindowController : NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    }
+
+}
