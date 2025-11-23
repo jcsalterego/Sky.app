2f2a460af07dfd05260306125a57ad3792f5601b
Jerry Chen <jerry@apache.org>
2025-11-23 09:32:54 -0600

    Ensure there's a min window size, and adjust content window size depending on an existing saved frame

--- Sky/MainWindowController.swift
+++ Sky/MainWindowController.swift
@@ -15,11 +15,41 @@ class MainWindowController : NSWindowController {
             return
         }
         let currentMinWidth = AppDelegate.shared.getUserDefaultMinimumWindowWidth()
-        let currentMinHeight = window.contentMinSize.height
-        window.contentMinSize = NSSize(
+        var currentMinHeight = window.contentMinSize.height
+        if currentMinHeight == 0 {
+            currentMinHeight = 667
+        }
+        let contentMinSize = NSSize(
             width: CGFloat(currentMinWidth),
             height: currentMinHeight
         )
+        window.contentMinSize = contentMinSize
+
+        let key = "NSWindow Frame MainWindow"
+        if let rectString = UserDefaults.standard.string(forKey: key) {
+            var rect = NSRectFromString(rectString)
+            if rect.height < contentMinSize.height {
+                rect = NSRect(
+                    x: rect.origin.x,
+                    y: rect.origin.y,
+                    width: rect.width,
+                    height: contentMinSize.height
+                )
+            }
+            if rect.width < contentMinSize.width {
+                rect = NSRect(
+                    x: rect.origin.x,
+                    y: rect.origin.y,
+                    width: contentMinSize.width,
+                    height: rect.height
+                )
+            }
+            window.setContentSize(rect.size)
+        } else {
+            window.setContentSize(contentMinSize)
+            window.center()
+        }
+
         let advancedSubmenu = NSApplication.shared.mainMenu?
             .item(withTitle: "Advanced")?.submenu
         let setUserDefaultMinimumWindowWidthSubmenu = advancedSubmenu?.item(withTitle: "Set Minimum Window Width")?.submenu
